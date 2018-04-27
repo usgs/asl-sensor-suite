52bc009a0fed3e0aad2b84af413e44c110fc506a
James Holland <jholland@usgs.gov>
2018-04-27 15:52:32 -0600

    Add more tests for ExperimentPanel
    Former-commit-id: d7417340ce6d3f6fdf6b90234cc984e300c9f6b0 [formerly f2ddf5940917d19fd3dcaafa4106257e288d2771] [formerly de008c8697627d2d3d70a5a0a1f626df8a27ae5a [formerly 798664c4f496fff1911166e06acb1b72ba443702]]
    Former-commit-id: 7075fd4d9d0952f6fad7c7f860214be5f00bb723 [formerly debc0b67f00d40c219f2baa38f0a3168e536fe04]
    Former-commit-id: 41d1528835d8e8cf6fd76b78352244b3c4162371

--- src/test/java/asl/sensor/gui/ExperimentPanelTest.java
+++ src/test/java/asl/sensor/gui/ExperimentPanelTest.java
@@ -1,18 +1,28 @@
 package asl.sensor.gui;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import asl.sensor.experiment.ExperimentEnum;
 import asl.sensor.input.DataStore;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ExperimentPanelTest {
 
+  ExperimentPanel panel;
+
+  @Before
+  public void setUp() {
+    this.panel = new MockPanel(ExperimentEnum.AZMTH);
+  }
+
   @Test
   public void buildChart_nullxyDataset_DefaultAxis() {
-    ExperimentPanel panel = new MockPanel(ExperimentEnum.AZMTH);
     //Since nothing else is initialized, this should just return without Exception.
     JFreeChart chart = panel.buildChart(null);
     assertNotNull(chart);
@@ -20,11 +30,46 @@ public class ExperimentPanelTest {
 
   @Test
   public void buildChart_nullxyDataset_AxisDefined() {
-    ExperimentPanel panel = new MockPanel(ExperimentEnum.AZMTH);
     //Since nothing else is initialized, this should just return without an Exception.
     JFreeChart chart = panel.buildChart(
         null, new NumberAxis("x Custom"), new NumberAxis("y Custom"));
     assertNotNull(chart);
+  }
+
+  @Test
+  public void setChart_nullxyDataset() {
+    //Since nothing else is initialized, this should just return without Exception.
+    panel.setChart(null);
+    assertNotNull(panel.chart);
+  }
+
+  @Test
+  public void hasRun_defaultValueIsFalse() {
+    assertFalse(panel.hasRun());
+  }
+
+  @Test
+  public void clearChart_setsHasRunToFalse() {
+    panel.updateData(null);
+    assertTrue(panel.hasRun());
+    panel.clearChart();
+    assertFalse(panel.hasRun());
+  }
+
+  @Test
+  public void getXAxis_returnsDefault() {
+    assertEquals("x Axis", panel.getXAxis().getLabel());
+  }
+
+  @Test
+  public void getYAxis_returnsDefault() {
+    assertEquals("y Axis", panel.getYAxis().getLabel());
+  }
+
+  @Test
+  public void getPDFFilename() {
+    assertTrue(panel.getPDFFilename().startsWith("Azimuth__"));
+    assertTrue(panel.getPDFFilename().endsWith(".pdf"));
   }
 
 
@@ -47,8 +92,8 @@ public class ExperimentPanelTest {
     }
 
     @Override
-    protected void updateData(DataStore ds) {
-
+    protected void updateData(DataStore dataStore) {
+      set = true;
     }
   }
 }
