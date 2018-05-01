cdb8344e753ef6af555b2dcedee9b772fe855af8
James Holland <jholland@usgs.gov>
2018-05-01 15:11:27 -0600

    Add some test util methods
    Former-commit-id: 27084be803ba3c3d6044573bd862c8834c9b7c75 [formerly bb42f1b4287559e9e3e2cfca500ea7cf71aa2c26] [formerly e44408f9ef160f265bf77a124babe080edd1d6a2 [formerly 3c3ee95dfed976229bc465b959be6a84e0ac18d4]]
    Former-commit-id: 396ca65a4df97d73c27580d3770121105ac65e6e [formerly 2e47333516878a265c54a0b0a86624bfc58c0947]
    Former-commit-id: 39103daa3833deaf73ff79ca9922a5af61b6af01

--- src/test/java/asl/sensor/test/TestUtils.java
+++ src/test/java/asl/sensor/test/TestUtils.java
@@ -12,7 +12,11 @@ public class TestUtils {
   // may need to change this to deal with eventual migration to main usgs github
   public static String SUBPAGE = "tests/";
   public static String TEST_DATA_LOCATION = "src/test/resources/seismic-test-data/";
-  public static DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("uuuu-DDD'T'HH:mm:ss.S");
+  public static String RESP_LOCATION = TEST_DATA_LOCATION + "RESPs/";
+
+  public static String getSeedFolder(String network, String station, String year, String dayOfYear) {
+    return String.format(TEST_DATA_LOCATION + "seed_data/%s_%s/%s/%s/", network, station, year, dayOfYear);
+  }
 
   public static OffsetDateTime getEndCalendar(DataStore ds) {
     Instant time = Instant.ofEpochMilli( ds.getBlock(0).getEndTime() );
@@ -25,6 +29,7 @@ public class TestUtils {
   }
 
   public static long timeStringToEpochMilli(String time) {
+    DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("uuuu-DDD'T'HH:mm:ss.S");
     return LocalDateTime.parse(time, DATE_TIME_FORMAT).toInstant(ZoneOffset.UTC).toEpochMilli();
   }
 
