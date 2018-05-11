1e1d1e164c89c8c438eefeaec3d56d2a4d65d8b2
James Holland <jholland@usgs.gov>
2018-05-11 17:33:00 -0600

    Add three tests for Experiment

--- src/test/java/asl/sensor/experiment/ExperimentTest.java
+++ src/test/java/asl/sensor/experiment/ExperimentTest.java
@@ -1,7 +1,38 @@
 package asl.sensor.experiment;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
 public class ExperimentTest {
 
-  // probably going to be used to test backend helper functions eventually
-  
+  @Test
+  public void experiment_constructorInitializes(){
+    Experiment experiment = new MockExperiment();
+    assertEquals(0, experiment.getStart());
+    assertEquals(0, experiment.getEnd());
+    assertTrue(experiment.getInputNames().isEmpty());
+    assertEquals("", experiment.getStatus());
+    assertNull(experiment.getGapRegions());
+  }
+
+  @Test
+  public void fireStateChange_updatesStatus(){
+    MockExperiment experiment = new MockExperiment();
+    experiment.fireStateChange("Fired Status Change");
+    assertEquals("Fired Status Change", experiment.getStatus());
+
+    assertEquals(1, experiment.numberOfChangesFired);
+  }
+
+  @Test
+  public void listActiveResponseIndices_defaultImplementation(){
+    Experiment experiment = new MockExperiment();
+    assertArrayEquals(new int[]{}, experiment.listActiveResponseIndices());
+  }
+
+
 }
