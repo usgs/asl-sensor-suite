ec4f77ecd62c0e994df58ef91fc94bd6d65c8b64
James Holland <jholland@usgs.gov>
2018-05-11 17:32:42 -0600

    Add MockExperiment class for test cases to use

---
+++ src/test/java/asl/sensor/experiment/MockExperiment.java
@@ -0,0 +1,41 @@
+package asl.sensor.experiment;
+
+import asl.sensor.input.DataStore;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class MockExperiment extends Experiment {
+  boolean backendCalled = false;
+  boolean hasEnoughDataCalled = false;
+  boolean blocksNeededCalled = false;
+  int numberOfChangesFired = 0;
+
+  MockExperiment() {
+    super();
+    this.addChangeListener(new changeCountingListener());
+  }
+
+  @Override
+  protected void backend(final DataStore dataStore) {
+    backendCalled = true;
+  }
+
+  @Override
+  public int blocksNeeded() {
+    blocksNeededCalled = true;
+    return 3;
+  }
+
+  @Override
+  public boolean hasEnoughData(DataStore dataStore) {
+    hasEnoughDataCalled = true;
+    return true;
+  }
+
+  private class changeCountingListener implements ChangeListener {
+    public void stateChanged(ChangeEvent event) {
+      numberOfChangesFired++;
+    }
+  }
+
+}
